527c7284b56f88d30b0160003e3ef7638d047573
chetan6667 <111834873+chetan6667@users.noreply.github.com>
2022-09-28 14:41:43 +0530

    Add files via upload

---
+++ q1.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    char n,(input);
+    cout<<"enter any alphabet : "<<endl;
+    cin>>n;
+    switch (n)
+    {
+    case 'a':
+    cout<<"it is a vowel"<<endl;
+    break;
+    case 'e':
+    cout<<"it is a vowel"<<endl;
+    break;
+    case 'i':
+    cout<<"it is a vowel"<<endl;
+    break;
+    case 'o':
+    cout<<"it is a vowel"<<endl;
+    break;
+    case 'u':
+    cout<<"it is a vowel"<<endl;
+    break;
+    case' A':
+    cout<<"it is a vowel"<<endl;
+    break;
+    case 'E':
+    cout<<"it is a vowel"<<endl;
+    break;
+    case 'I':
+    cout<<"it is a vowel"<<endl;
+    break;
+    case 'O':
+    cout<<"it is a vowel"<<endl;
+    break;
+    case 'U':
+    cout<<"it is a vowel"<<endl;
+    break;
+    default :
+     cout<<"consonent"<<endl;
+        }
+     return 0;
+}
